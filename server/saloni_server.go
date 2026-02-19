6d625f5161cc352913ecd839c0087c76d31523c8
Prerana Anand <preranaanand30@gmail.com>
2026-02-18 19:54:53 -0800

    editing server - P

--- server/saloni_server.go
+++ server/saloni_server.go
@@ -10,15 +10,20 @@ import (
 	"log"
 	"net"
 	"os"
+	"strings"
 )
 
 func handleStorage(msgHandler *messages.MessageHandler, request *messages.StorageRequest) error {
-	log.Printf("Storing %q (%d bytes)", request.FileName, request.Size)
+	fileName := request.FileName
+	splitList := strings.Split(fileName, "/")
+	fileName = splitList[len(splitList)-1]
 
-	file, err := os.OpenFile(request.FileName, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0666)
+	log.Printf("Storing %q (%d bytes)", fileName, request.Size)
+
+	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0666)
 	if err != nil {
 		msgHandler.SendResponse(false, err.Error())
-		return fmt.Errorf("open %q: %w", request.FileName, err)
+		return fmt.Errorf("open %q: %w", fileName, err)
 	}
 	defer file.Close()
 
@@ -26,25 +31,25 @@ func handleStorage(msgHandler *messages.MessageHandler, request *messages.Storag
 
 	h := md5.New()
 	if _, err := io.CopyN(io.MultiWriter(file, h), msgHandler, int64(request.Size)); err != nil {
-		os.Remove(request.FileName)
+		os.Remove(fileName)
 		return fmt.Errorf("receiving data: %w", err)
 	}
 
 	clientCheckMsg, err := msgHandler.Receive()
 	if err != nil {
-		os.Remove(request.FileName)
+		os.Remove(fileName)
 		return fmt.Errorf("receiving checksum: %w", err)
 	}
 
 	clientCheck := clientCheckMsg.GetChecksum().Checksum
 	if !util.VerifyChecksum(h.Sum(nil), clientCheck) {
-		os.Remove(request.FileName)
+		os.Remove(fileName)
 		msgHandler.SendResponse(false, "Checksum mismatch")
 		return errors.New("checksum mismatch — file removed")
 	}
 
 	msgHandler.SendResponse(true, "File stored successfully")
-	log.Printf("Stored %q successfully", request.FileName)
+	log.Printf("Stored %q successfully", fileName)
 	return nil
 }
 
